78f4c9d8d7b7d1d9b314814f47f7cf572dd260ac
Marina Diubailo <marinadiubailo@gmail.com>
2024-10-04 13:14:06 +0300

    feat: add streams module task solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,22 @@
+import { createReadStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { stdout } from 'node:process';
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const path = resolve(__dirname, 'files', 'fileToRead.txt');
+
+  const stream = createReadStream(path, {
+    encoding: 'utf-8',
+  });
+
+  try {
+    await pipeline(stream, stdout);
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,24 @@
+import { pipeline } from 'node:stream/promises';
+import { createWriteStream } from 'node:fs';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { stdin } from 'node:process';
+
 const write = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const path = resolve(__dirname, 'files', 'fileToWrite.txt');
+
+  const stream = createWriteStream(path);
+
+  try {
+    await pipeline(stdin, stream);
+  } catch (e) {
+    console.error(e);
+  }
+
+  stream.on('finish', () => {
+    console.log('Done');
+  });
 };
 
 await write();
