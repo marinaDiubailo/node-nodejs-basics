c2ee19653086913b8ddf8d81cc38206e9a086602
Marina Diubailo <marinadiubailo@gmail.com>
2024-10-04 13:13:32 +0300

    feat: add zip module task solution

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,27 @@
+import { pipeline } from 'node:stream/promises';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGunzip } from 'node:zlib';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const decompress = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const compressedFilePath = resolve(__dirname, 'files', 'archive.gz');
+  const decompressedFilePath = resolve(
+    __dirname,
+    'files',
+    'fileToCompress.txt',
+  );
+
+  try {
+    await pipeline(
+      createReadStream(compressedFilePath),
+      createGunzip(),
+      createWriteStream(decompressedFilePath),
+    );
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 await decompress();
