9353364e6663c242dbf04d64d58b1e450073f0bb
Marina Diubailo <marinadiubailo@gmail.com>
2024-10-05 13:10:41 +0300

    feat: add cli module task solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+  const result = [];
+  const [executor, file, ...args] = argv;
+
+  for (let i = 0; i < args.length; i += 2) {
+    const propName = args[i].substring(2);
+    const value = args[i + 1];
+    result.push(`${propName} is ${value}`);
+  }
+
+  console.log(result.join(', '));
 };
 
 parseArgs();
