642e7c691dd259a5f6bf060de382dfac0f8bb3e2
Marina Diubailo <marinadiubailo@gmail.com>
2024-10-05 09:10:40 +0300

    feat: add hash module task solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { createReadStream } from 'node:fs';
+import { resolve, dirname } from 'node:path';
+import { pipeline } from 'node:stream/promises';
+import { fileURLToPath } from 'node:url';
+const { createHash } = await import('node:crypto');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const path = resolve(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+  const stream = createReadStream(path);
+  const hash = createHash('sha256');
+
+  try {
+    await pipeline(stream, hash);
+
+    const result = hash.digest('hex');
+    console.log(result);
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 await calculateHash();
